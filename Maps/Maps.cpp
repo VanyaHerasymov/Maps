01440a2e0ab472f035ba43d91fae18a9bb27ee41
HerasymovIvan <gerasymov.ivan@gmail.com>
2024-12-18 17:42:51 +0200

    Add project files.

---
+++ Maps/Maps.cpp
@@ -0,0 +1,57 @@
+// Maps.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <map>
+#include <string>
+#include "Date.h"
+using namespace std;
+
+int main()
+{
+    /*map<string, int> words;
+
+    string user;
+    while (user != "stop") {
+        cout << "Input a word: ";
+        cin >> user;
+        if (user == "stop")break;
+
+        if (words.find(user) == words.end()) {
+
+            //words.insert(pair<string, int>(user, 1));
+            words[user] = 1;
+        
+        }
+        else {
+            words[user]++;
+        }
+
+    }
+        cout << "---------------------"<<endl;
+    
+        
+        
+    for (auto& item : words)
+    {
+        cout << item.first << " " << item.second << endl;
+
+
+
+    }*/
+
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
