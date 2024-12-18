035d323e39e4fabc17d057b8ce4d33474216c913
HerasymovIvan <gerasymov.ivan@gmail.com>
2024-12-18 18:23:36 +0200

    Definition

--- Maps/Diary.cpp
+++ Maps/Diary.cpp
@@ -29,8 +29,35 @@ void Diary::addEvent(Date D, string info)
 
 void Diary::findByDate(Date D)
 {
+	for (auto item : book) {
+		if(item.first==D){
+			cout << item.first << endl;
+			for (int i = 0; i < item.second.size(); i++) {
+				cout << "\t" << item.second[i] << endl;
+
+			}cout << endl;
+		}
+		else {
+			cout << "there are no events with this Date";
+		}
+	}
 }
 
 void Diary::finByEvent(string info)
 {
+	/*for (auto item : book) {
+		
+		if(item.book)
+		
+		{
+			cout << item.first << endl;
+			for (int i = 0; i < item.second.size(); i++) {
+				cout << "\t" << item.second[i] << endl;
+
+			}cout << endl;
+		}
+		else {
+			cout << "there are no events with this name";
+		}
+	}*/
 }
